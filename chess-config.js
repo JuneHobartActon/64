c9ef2cfb2f40bde7f0de961c2e9a1659498cad88
JuneHobartActon <junehobart@hexiq.com>
2025-01-06 07:21:26 -0500

    Create chess-config.js

---
+++ chess-config.js
@@ -0,0 +1,140 @@
+// Chess piece information configuration
+const pieceInfo = {
+    // Black pawns (numbered 1-8 from left to right)
+    'black-pawn-1': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-pawn-2': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-pawn-3': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-pawn-4': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-pawn-5': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-pawn-6': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-pawn-7': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-pawn-8': {
+        title: '',
+        description: '',
+        position: ''
+    },
+
+    // White pawns (numbered 1-8 from left to right)
+    'white-pawn-1': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-pawn-2': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-pawn-3': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-pawn-4': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-pawn-5': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-pawn-6': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-pawn-7': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-pawn-8': {
+        title: '',
+        description: '',
+        position: ''
+    },
+
+    // Other white pieces
+    'white-king': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-queen': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-rook': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-bishop': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'white-knight': {
+        title: '',
+        description: '',
+        position: ''
+    },
+
+    // Other black pieces
+    'black-king': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-queen': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-rook': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-bishop': {
+        title: '',
+        description: '',
+        position: ''
+    },
+    'black-knight': {
+        title: '',
+        description: '',
+        position: ''
+    }
+};
